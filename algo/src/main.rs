e3e1bc5d177336ba11a195ce9b7710f08380ecc7
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-04-29 21:13:08 +0530

    Added binary search function

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,12 +1,26 @@
 fn main() {
-    println!("Binary Search!");
-    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-    println!("The array is {:?}", arr);
-    println!("The length of the array is: {}", arr.len());
-    let mut mid = arr.len() / 2;
-    println!("The middle element index is {}", mid);
-    println!("The middle element is {}", arr[mid]);
-    for item in 0..arr.len() {
-        println!("{}", arr[item]);
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]; // Sorted array
+    binary_search(20, arr);
+    binary_search(8, arr);
+}
+fn binary_search(key: i32, arr: [i32; 9]) {
+    let mut left = 0;
+    let mut right = arr.len() - 1;
+    while right >= left {
+        let mid = left + (right - left) / 2; // Defining the mid element
+        if arr[mid] == key {
+            // If the element is present at mid itself
+            println!("Element found at index {}", mid);
+            break;
+        } else if arr[mid] > key {
+            // If mid is greater, Then moving to the left sub array
+            right = mid - 1;
+        } else {
+            // If mid is lower, Then moving to the right sub array
+            left = mid + 1;
+        }
+    }
+    if left > right {
+        println!("Element is not present in the array")
     }
 }
