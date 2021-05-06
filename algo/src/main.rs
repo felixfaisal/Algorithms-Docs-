70d1ad8dd6ddab1b36135362961627e015be101d
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-05-07 00:10:45 +0530

    Adding QuickSort Algorithm

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,4 +1,12 @@
 mod sort;
+fn partition(arr:[i32;7],left:i32,right:i32) -> {
+    
+}
+fn quick_sort(arr:[i32;7],left:i32,right:i32) -> {
+    if low<high{
+        let pi = partition(arr,low,high);
+    }
+}
 fn main() {
     let mut arr = [64, 34, 25, 8, 22, 11, 9];
     arr = sort::merge_sort(arr, 0, arr.len());
