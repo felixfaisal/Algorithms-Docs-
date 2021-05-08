8d8b1dcb00c100f6d4b421c8bf5106d718a23048
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-05-08 22:49:48 +0530

    Working with structs

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,38 +1,23 @@
 mod sort;
-fn partition(mut arr: [i32; 7], left: usize, right: usize) -> (usize, [i32; 7]) {
-    // pivot (Element to be placed at right position)
-    let pivot = arr[right];
-    let leftI = left as i32;
-    let mut i: i32 = leftI - 1;
-    for j in left..right {
-        if arr[j] < pivot {
-            i = i + 1; // increment index of smaller element
-            let temp = arr[i as usize];
-            arr[i as usize] = arr[j];
-            arr[j] = temp; // swap arr[i] and arr[j]
-        }
-    }
-    // swap arr[i + 1] and arr[high])
-    let finalpos = i + 1;
-    let temp = arr[right];
-    arr[right] = arr[finalpos as usize];
-    arr[finalpos as usize] = temp;
-    (finalpos as usize, arr)
-}
-
-fn quick_sort(mut arr: [i32; 7], left: usize, right: usize) -> [i32; 7] {
-    if left < right {
-        // let pi: usize;
-        let (pi, x) = partition(arr, left, right);
-        arr = x.clone();
-        arr = quick_sort(arr, left, pi - 1); // Before pi
-        arr = quick_sort(arr, pi, right); // After pi
-    }
-    arr
+// Initializing a Structure
+struct Student {
+    name: String,
+    age: u16,
 }
 fn main() {
-    let mut arr = [64, 34, 25, 8, 22, 11, 9];
-    // arr = sort::merge_sort(arr, 0, arr.len());
-    arr = quick_sort(arr, 0, arr.len() - 1);
-    println!("Sorted array is {:?}", arr);
+    let faisal = Student {
+        name: String::from("Faisal"),
+        age: 21,
+    };
+    let students: [Student; 2] = [
+        Student {
+            name: String::from("Felix"),
+            age: 21,
+        },
+        Student {
+            name: String::from("Mary"),
+            age: 21,
+        },
+    ];
+    println!("Name: {} \nAge: {}", faisal.name, faisal.age);
 }
--- algo/src/sort.rs
+++ algo/src/sort.rs
@@ -116,3 +116,34 @@ fn merge(mut arr: [i32; 7], left: usize, mid: usize, right: usize) -> [i32; 7] {
     }
     arr
 }
+pub fn partition(mut arr: [i32; 7], left: usize, right: usize) -> (usize, [i32; 7]) {
+    // pivot (Element to be placed at right position)
+    let pivot = arr[right];
+    let leftI = left as i32;
+    let mut i: i32 = leftI - 1;
+    for j in left..right {
+        if arr[j] < pivot {
+            i = i + 1; // increment index of smaller element
+            let temp = arr[i as usize];
+            arr[i as usize] = arr[j];
+            arr[j] = temp; // swap arr[i] and arr[j]
+        }
+    }
+    // swap arr[i + 1] and arr[high])
+    let finalpos = i + 1;
+    let temp = arr[right];
+    arr[right] = arr[finalpos as usize];
+    arr[finalpos as usize] = temp;
+    (finalpos as usize, arr)
+}
+
+pub fn quick_sort(mut arr: [i32; 7], left: usize, right: usize) -> [i32; 7] {
+    if left < right {
+        // let pi: usize;
+        let (pi, x) = partition(arr, left, right);
+        arr = x.clone();
+        arr = quick_sort(arr, left, pi - 1); // Before pi
+        arr = quick_sort(arr, pi, right); // After pi
+    }
+    arr
+}
