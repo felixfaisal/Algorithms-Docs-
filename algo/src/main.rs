412c42d25845e5e96908beae50c800c22dc4919d
felixfaisal <faisalahmedfarooq46@gmail.com>
2021-05-12 13:00:31 +0530

    Added linked list append with struct

--- algo/src/main.rs
+++ algo/src/main.rs
@@ -1,19 +1,66 @@
 mod linkedlist;
-use crate::linkedlist::List;
+struct myList {
+    value: u32,
+    next: address,
+}
+enum address {
+    address(Box<myList>),
+    Nil,
+}
+impl myList {
+    fn new(elem: u32) -> myList {
+        myList {
+            value: elem,
+            next: address::Nil,
+        }
+    }
+    fn append(mut self, elem: u32) -> myList {
+        let node = myList {
+            value: elem,
+            next: address::Nil,
+        };
+        self.next = address::address(Box::new(node));
+        self
+    }
+    fn append2(&mut self, elem: u32) {
+        match self.next {
+            address::address(ref mut nextaddress) => {
+                nextaddress.append2(elem);
+            }
+            address::Nil => {
+                let node = myList {
+                    value: elem,
+                    next: address::Nil,
+                };
+                self.next = address::address(Box::new(node))
+            }
+        }
+    }
+    fn list(&self) {
+        println!("{}", self.value);
+        match self.next {
+            address::address(ref nextaddress) => {
+                nextaddress.list();
+            }
+            address::Nil => {
+                println!("Nil")
+            }
+        }
+    }
+}
 fn main() {
-    // Create an empty linked list
-    let mut list = List::new();
+    let mut head = myList {
+        value: 8,
+        next: address::Nil,
+    };
+    head.append2(15);
+    head.append2(19);
+    head.list();
 
-    // Prepend some elements
-    list = list.prepend(1);
-    list = list.prepend(2);
-    list = list.prepend(3);
-    list = list.prepend(7);
-    list = list.prepend(6);
-    list.append(8);
-    list.append(9);
-
-    // Show the final state of the list
-    println!("linked list has length: {}", list.len());
-    println!("{}", list.stringify());
+    match head.next {
+        address::address(ref addressX) => {
+            println!("This has a next address containing, {}", addressX.value);
+        }
+        address::Nil => {}
+    }
 }
